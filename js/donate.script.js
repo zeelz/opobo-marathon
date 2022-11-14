cd88cd5a6c1caa2a9402d7af2f8a633d4778961d
Keme Kenneth <keme.kenneth@gmail.com>
2022-11-14 09:45:12 +0100

    Live payment

--- js/donate.script.js
+++ js/donate.script.js
@@ -1,6 +1,6 @@
 function payWithPaystack(email, amount) {
     let handler = PaystackPop.setup({
-        key: 'pk_test_0337426f78356df028480c35319a44428f71883d', // Replace with your public key
+        key: 'pk_live_49853b01d14ce2df941d5e51fd8f8ec443d2b28d',
         email,
         amount: amount * 100,
         ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
@@ -11,8 +11,21 @@ function payWithPaystack(email, amount) {
         },
 
         callback: function(response){
-            let message = 'Payment complete! Reference: ' + response.reference;
-            alert(message);
+            // let message = 'Payment complete! Reference: ' + response.reference;
+            // {
+            //     "reference": "284009046",
+            //     "trans": "2282496436",
+            //     "status": "success",
+            //     "message": "Approved",
+            //     "transaction": "2282496436",
+            //     "trxref": "284009046",
+            //     "fallback": false,
+            //     "bank": "UNITED BANK FOR AFRICA",
+            //     "return": "{\"redirecturl\":\"?trxref=284009046&reference=284009046\",\"trans\":\"2282496436\",\"trxref\":\"284009046\",\"reference\":\"284009046\",\"status\":\"success\",\"message\":\"Success\",\"response\":\"Approved\",\"fallback\":false,\"bank\":\"UNITED BANK FOR AFRICA\"}",
+            //     "redirecturl": "?trxref=284009046&reference=284009046"
+            // }
+            // console.log(response);
+            $('#thankyouModal').modal('show')
         }
     });
 
@@ -27,8 +40,8 @@ document.querySelector('#donateForm').addEventListener('submit', function(e){
     e.preventDefault()
 
     const email = e.target.email.value
-    const amount = donateAmount.split(',').join('')
-    // console.log(email, amount)
+    let amount = e.target.setAmount.value
+    amount = !amount? donateAmount.split(',').join('') : amount
     payWithPaystack(email, amount)
     
 
@@ -39,6 +52,7 @@ const btns = document.querySelectorAll('#donateForm button')
 btns.forEach( btn => {
     btn.addEventListener('click', e => {
         e.preventDefault()
+        document.querySelector('#setAmount').value = ''
         donateAmount = e.target.textContent
         btns.forEach( btn => btn.classList.remove('active'))
         e.target.classList.add('active')
