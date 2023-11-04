4e5182b4fb0207191f05505de8efbdb19e1b9221
Keme Kenneth <keme.kenneth@gmail.com>
2023-11-04 06:36:05 +0100

    2024

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <?php $page = 'home'; ?>
 
 <head>
-    <title>Home - Opobo Marathon 2023</title>
+    <title>Home - Opobo Marathon 2024</title>
     <?php include('./partials/head.php'); ?>
 </head>
 
